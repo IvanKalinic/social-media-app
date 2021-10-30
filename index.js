76dc47ce4606363c883fec48719b99e5f886a4b0
Ivan Kalinić <ivankalinic1998@gmail.com>
2021-10-30 16:31:18 +0200

    Added auth routes for register and login

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ const authRoute = require("./routes/auth");
 
 dotenv.config();
 
-mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("Connected to mongo");
-  }
-);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
 
 //middleware
 app.use(express.json());
